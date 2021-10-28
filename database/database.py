445cc486fa229ab8281980e539ff39f986a54bfa
Liem Eldert <liemeldert@protonmail.com>
2021-10-28 00:48:20 -0400

    database imp

---
+++ database/database.py
@@ -0,0 +1,32 @@
+import datetime
+from typing import Optional
+from pydantic import BaseModel
+from beanie import Document, Indexed, init_beanie
+import asyncio, motor
+from main import bot
+
+
+class Server(Document):
+    _server_id: int
+    preferred_name: str
+    roles: dict
+
+    # IMAGE FILTERING
+    image_filtering: bool
+    image_filtering_channels: list
+    image_filtering_roles: list
+    image_filtering_nsfw: bool
+
+    # Misc
+    enable_amptuator: bool
+    disabled_modules: list
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        server = bot.get_guild(self._server_id)
+
+
+class User(Document):
+    name: str
+    pronouns: str
+    birthday: datetime.datetime
--- main.py
+++ main.py
@@ -5,25 +5,30 @@
 import disnake
 from disnake.ext import commands
 from zoidberg.config import *
+import motor.motor_asyncio
+
 
 # define activity, playing status
 activity = disnake.Activity(name='> planetexpresslabs.io', type=disnake.ActivityType.playing)
 # define gateway intents
 intents = disnake.Intents.default()
 intents.members = True
-
+print(TEST_GUILDS)
 bot = commands.Bot(command_prefix='-=',
                    activity=activity,
                    intents=intents,
-                   test_guilds=TEST_GUILDS)
+                   test_guilds=[842987183588507670])
 
-#enable torturing if you hate yourself, and everyone in your server
-disabled_cogs = ['torture.py']
+# enable torturing if you hate yourself, and everyone in your server
+disabled_cogs = ['torture.py', 'image_filtering.py']
 logging.basicConfig(level=logging.INFO)
-
 for filename in os.listdir("cogs"):
     if filename.endswith(".py") and filename not in disabled_cogs:
         bot.load_extension(f"cogs.{filename[:-3]}")
+if DB_LOCALHOST:
+    mongo_client = motor.motor_asyncio.AsyncIOMotorClient('localhost', 27017)
+else:
+    mongo_client = motor.motor_asyncio.AsyncIOMotorClient(CONNURL)
 
 
 @bot.event
