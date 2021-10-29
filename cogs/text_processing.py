ab598cc9a64588e9b9a9ce534984e28ed7e44788
Liem Eldert <liemeldert@protonmail.com>
2021-10-28 22:39:28 -0400

    cleaning

--- cogs/text_processing.py
+++ cogs/text_processing.py
@@ -9,11 +9,14 @@ from utils.languagetool_utils import get_matches, correct
 import requests
 from async_google_trans_new import AsyncTranslator
 from async_google_trans_new.constant import LANGUAGES
+import pycountry
+
 
 class Text_Processor(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.ltool_codes = ['auto']+[lang['longCode'].lower() for lang in requests.get("https://api.languagetoolplus.com/v2/languages").json()]
+        self.ltool_codes = ['auto'] + [lang['longCode'].lower() for lang in
+                                       requests.get("https://api.languagetoolplus.com/v2/languages").json()]
         self.translate_langs = LANGUAGES
         self.translator = AsyncTranslator()
 
@@ -22,41 +25,47 @@ class Text_Processor(commands.Cog):
         description="Proofreads and replies with changed text.",
         options=[
             Option("text", "The text to proofread", OptionType.string, required=True),
-            Option("lang", "Language to proofread in (default: en-US)", OptionType.string)
+            Option("target", "Language to proofread in (default: en-US)", OptionType.string)
         ]
     )
-    async def proofread(self, ctx, text, lang="auto"):
+    async def proofread(self, ctx, text, target="auto"):
         prefix = ''
-        if lang != "auto":
-            lang = lang.lower()
-            if not lang in self.ltool_codes:
-                prefix = f"It seems that your selected language ({lang}) is not supported. Language codes should be formatted by locale (eg. en-US or fr). Falling back to Automatic.\n\n"
-                lang = "auto"
-        matches = await get_matches(text, lang)
+        if target != "auto":
+            langs = pycountry.languages
+            target = langs.lookup(target).alpha_2
+            if target not in self.ltool_codes:
+                prefix = f"It seems that your selected language ({target}) is not supported. Language codes should be " \
+                         f"formatted by locale (eg. en-US or fr). Falling back to Automatic.\n\n "
+                target = "auto"
+        matches = await get_matches(text, target)
         if len(matches) == 0:
-            await ctx.response.send_message(prefix+"No problems were detected with your text!")
+            await ctx.response.send_message(prefix + "No problems were detected with your text!")
             return
         corrected = correct(text, matches)
-        await ctx.response.send_message(prefix+corrected)
+        await ctx.response.send_message(prefix + corrected)
 
     @commands.slash_command(
         name="translate",
         description="Translates text to a new language!",
         options=[
             Option("text", "The text to translate", OptionType.string, required=True),
-            Option("lang", "Language to translate to (default: en)", OptionType.string)
+            Option("target", "Language to translate to (default: en)", OptionType.string)
         ]
     )
-    async def translate(self, ctx, text, lang="en"):
+    async def translate(self, ctx, text, target="en"):
+        langs = pycountry.languages
+        target = langs.lookup(target).alpha_2
         prefix = ''
-        if lang != "en":
-            lang = lang.lower()
-            if lang not in self.translate_langs:
-                prefix = f"It seems your selected language ({lang}) is not supported. Language codes should be formatted by 2-letter locale (eg. en or fr). Falling back to English.\n\n"
-                lang = "en"
-        text = await self.translator.translate(text, lang)
+        if target != "en":
+            target = target.lower()
+            if target not in self.translate_langs:
+                prefix = f"It seems your selected language ({target}) is not supported. Language codes should be " \
+                         f"formatted by 2-letter locale (eg. en or fr). Falling back to English.\n\n "
+                target = "en"
+        text = await self.translator.translate(text, target)
         await ctx.response.send_message(prefix + text)
 
+    # PING REPLIES
     @commands.command(name="proofread")
     async def proofread(self, ctx):
         ref = ctx.message.reference
@@ -82,6 +91,7 @@ class Text_Processor(commands.Cog):
                 return
             translated = await self.translator.translate(text, "en")
             await replied_to.reply(translated, mention_author=False)
+
 
 def setup(bot):
     bot.add_cog(Text_Processor(bot))
