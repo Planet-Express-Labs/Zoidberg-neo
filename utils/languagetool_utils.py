0f4fa7db9c00d50f1c115163710a588190ffeef8
Liem Eldert <liemeldert@protonmail.com>
2021-10-29 09:53:42 -0400

    fixed ssl error

--- utils/languagetool_utils.py
+++ utils/languagetool_utils.py
@@ -4,25 +4,29 @@
 # As long as you are using my code in good faith, we will probably not have an issue with it.
 import requests_async
 
+
 class Match:
     def __init__(self, match_dict):
         self.replacements = match_dict.get('replacements')
         self.offset = match_dict.get('offset')
         self.errorLength = match_dict.get('length')
 
+
 async def get_matches(text, lang="en-US"):
-    response = await requests_async.get("https://api.languagetoolplus.com/v2/check", params={"text":text, "language":lang})
+    response = await requests_async.get("https://api.languagetoolplus.com/v2/check",
+                                        params={"text": text, "language": lang}, verify=False)
     query_dict = response.json()
     return [Match(match) for match in query_dict['matches'] if match.get('replacements')]
 
+
 def correct(text, matches):
-    text_list=list(text)
-    errors = [text_list[match.offset:match.offset+match.errorLength] for match in matches]
-    offset=0
+    text_list = list(text)
+    errors = [text_list[match.offset:match.offset + match.errorLength] for match in matches]
+    offset = 0
     for i, match in enumerate(matches):
         f = offset + match.offset
         t = f + match.errorLength
-        if text_list[f:t]!=errors[i]:
+        if text_list[f:t] != errors[i]:
             continue
         replacement = list(f"**{match.replacements[0]['value']}**")
         text_list[f:t] = replacement
