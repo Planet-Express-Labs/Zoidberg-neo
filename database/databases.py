fa4500910f4c9f17d8d70d9fd3e76175fabced20
Liem Eldert <liemeldert@protonmail.com>
2021-11-24 00:42:32 -0500

    Added fields

--- database/databases.py
+++ database/databases.py
@@ -2,33 +2,52 @@ import datetime
 from typing import Optional
 from pydantic import BaseModel
 from beanie import Document, Indexed, init_beanie
-import asyncio, motor
+import pymongo
 
 
 class Server(Document):
+    """Stores information specific to a discord guild/server. 
+    """
     server_id: int
     preferred_name: str
     roles: dict
+    premium: int
 
-    # IMAGE FILTERING
+    ### Moderation
+
+    # Image Filtering
     image_filtering: bool
     image_filtering_channels: list
+    image_filtering_channels_blocklist: bool
     image_filtering_roles: list
+    image_filtering_roles_blocklist: bool
     image_filtering_nsfw: bool
 
-    # TEXT FILTERING
+    # Text Filtering
     filter_long_text: bool
     filter_long_text_limit: int = 100
 
-    # Misc
+    # Server sync
+
+    ### Misc
     enable_amptuator: bool
     disabled_modules: list
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
 
 class User(Document):
+    """
+    Main document for storing information specific to users. 
+    """
+    id: str
     name: str
     pronouns: str
+    gender: str
     birthday: datetime.datetime
+    hidden_users: list
+    premium: int
+
+
+class Ban(Document):
+    """
+    Document for storing banned users. 
+    """
